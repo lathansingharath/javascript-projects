ebf9a2ad1d8591b30863b4dbdcbc907e6df02c29
Lathansingharath <singharath@hotmail.com>
2024-01-10 22:11:47 -0600

    Solved logic errors

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -4,25 +4,44 @@
 
 let launchReady = false;
 let fuelLevel = 17000;
+let fuelReady = false;
 let crewStatus = true;
+let crewReady = false;
 let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+ //  launchReady = true;
+   fuelReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+  // launchReady = false;
+   fuelReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("fuelReady = ", fuelReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+}
+
+console.log("crewReady = ", crewReady);
+
+if (fuelReady && crewReady) {
+   launchReady = true;
+} else {
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("lauchReady =",launchReady);
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
