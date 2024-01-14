8794cfa67518fc06ed0fb7d3d99ebeb1fbf448a5
Lathansingharath <singharath@hotmail.com>
2024-01-13 22:45:39 -0600

    Finished code snippets

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -1,4 +1,6 @@
 let jsCreator = "Brendan Eich";
 
+
+console.log("The word",jsCreator,"is",jsCreator.length,"letters long.");
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,10 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "Him";
+let name = "John";
+let verb = "drive";
+let adjective = "buffy";
+let color = "blue";
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+
+//The next block of code will (print the same thing but with template literals
+console.log(`JavaScript provides a ${color} collection of tools - inxlusinf ${adjective} syntax and ${pluralNoun} - that allows ${name} to ${verb} with strings.`)
