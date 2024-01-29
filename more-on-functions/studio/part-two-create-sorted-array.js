31cf34b76e9b9100af92e22fe8d361337e55b822
Lathan Singharath <singharath@hotmail.com>
2024-01-29 06:24:52 -0600

    spacing

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -36,6 +36,7 @@ function sortArr (arr) {
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
