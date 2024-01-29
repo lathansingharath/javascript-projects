7da376559f56261b041f0bd7dd56baae24b41b3b
Lathan Singharath <singharath@hotmail.com>
2024-01-28 23:32:53 -0600

    changing parameter

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -41,4 +41,4 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-console.log(sortArr(nums3));
+console.log(sortArr(nums1));
