39b6317ea8963c0e42e0d285e235b15f41aabf24
Lathansingharath <singharath@hotmail.com>
2024-02-03 08:16:15 -0600

    committing

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,10 @@
 // Import the modules exported from practiceExports.js below:
+
+const practice = require("./practiceExports.js");
+
+// console.log(patrice.isPalindrome("jack"))
+console.log(practice);
+console.log(typeof practice);
+// console.log(typeof palindromeCheck);
+// console.log(palindromeCheck("jack"));
+// console.log(palindromeCheck("radar"));
