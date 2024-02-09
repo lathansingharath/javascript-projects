ab5afb2a62a9e34a6b82146565598002b3fab2b7
Lathansingharath <singharath@hotmail.com>
2024-02-09 01:26:01 -0600

    tested

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,44 @@
 
 let launchcode = {
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["java","science","math"],
   
-}
+};
 
-module.exports = launchcode;
+const launchOutput = (num) => {
+    if (num % 2 === 0) {
+        if (num % 3 === 0){
+            if (num % 5 ===0) {
+                return "LaunchCode  Rocks!";
+            } else {
+                return "LaunchCode!";
+            }
+           
+        } else if (num % 5 === 0) {
+            return "Launch Rocks! (CRASH!!!!)";
+        } else {
+            return "Launch!";
+        }
+  
+    }
+    if (num % 3 === 0) {
+        if (num % 5 === 0) {
+            return "Code Rocks!";
+        } else {
+            return "Code!";
+        }
+    }
+    if (num % 5 === 0) {
+        return "Rocks!";
+    }
+     
+    return "Rutabagas! That doesn't work.";
+};
+
+module.exports = {
+    launchcode: launchcode,
+    launchOutput: launchOutput,
+}
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,53 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const launching = require('../index.js');
+//const launchOutput = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function() {
 
   // Write your unit tests here!
+  test("Check to see if organization is nonprofit", function () {
+    expect(launching.launchcode.organization).toEqual("nonprofit");
+  });
+  test ("Check to the executive director is Jeff ",function () {
+    expect(launching.launchcode.executiveDirector).toEqual("Jeff");
+  });
+  test("The percentage of cool employees should be 100% ", function () {
+    expect(launching.launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test("Should have java, science, and math as the offered programs ", function() {
+    expect(launching.launchcode.programsOffered[0]).toBe("java");
+    expect(launching.launchcode.programsOffered[1]).toBe("science");
+    expect(launching.launchcode.programsOffered[2]).toBe("math");
+    expect((launching.launchcode.programsOffered).length).toBe(3);
+
+  });
   
+  test("Number is divisible by 2", function () {
+    expect(launching.launchOutput(4)).toEqual("Launch!");
+  });
+  test("Number is divisible by 3", function () {
+    expect(launching.launchOutput(9)).toEqual("Code!");
+  });
+  test("Number is divisible by 5", function () {
+    expect(launching.launchOutput(25)).toEqual("Rocks!");
+  });
+
+  test("Number is divisible by 2 and 3 ", function() {
+    expect(launching.launchOutput(6)).toEqual("LaunchCode!");
+  });
+  test("Number is divisible by 3 and 5 ", function() {
+    expect(launching.launchOutput(15)).toEqual("Code Rocks!");
+  });
+  test("Number is divisible by 2 and 5 ", function() {
+    expect(launching.launchOutput(10)).toEqual("Launch Rocks! (CRASH!!!!)");
+  });
+  test("Number is divisible by 2 and 3 and 5 ", function() {
+    expect(launching.launchOutput(30)).toEqual("LaunchCode  Rocks!");
+  });
+  test("Number is not divisible by 2 or 3 or 5 ", function() {
+    expect(launching.launchOutput(1)).toEqual("Rutabagas! That doesn't work.");
+  });
+
+
+
 });
