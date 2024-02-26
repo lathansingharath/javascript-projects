648a6d69b86eda317838d57bd01487f4b4a992e3
Lathansingharath <singharath@hotmail.com>
2024-02-25 19:15:34 -0600

    shuttle cofirmed takeoff

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,29 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click",function(even){
+        paragraph.innerHTML = "Houston, we have liftoff!";
+
+    });
+    missionAbort.addEventListener("mouseover", function(event){
+        
+        missionAbort.style.backgroundColor = "red";
+      
+    });
+    missionAbort.addEventListener("mouseout",function(event) {
+        missionAbort.style.backgroundColor ="";
+    });
+    missionAbort.addEventListener("click", function(event){
+        let abortConfirm = window.confirm("Are you sure you want to abort the mission?");
+        if (abortConfirm) {
+            window.alert("Mission aborted! Space shuttle returning home");
+        }
+        
+
+    });
+
+
+
     
 }
 
