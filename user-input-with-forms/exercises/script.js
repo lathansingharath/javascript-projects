c64849faade0d4983123f0224556e6c99420f4a5
Lathansingharath <singharath@hotmail.com>
2024-02-28 22:38:22 -0600

    done with exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,39 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+
+    let submitButton = document.getElementById("runSim");
+    let form = document.getElementById("formID");
+    //let inputName = document.getElementById("tName");
+    let inputName = document.querySelector("input[name=testName");
+    //let inputDate = document.getElementById("tDate");
+    let inputDate = document.querySelector("input[name=testDate");
+    
+
+   
+    form.addEventListener("submit", function(){
+        
+        if (inputName.value==="") {
+            window.alert("Name cannot be blank");
+            
+        }
+        if (inputDate.value ===""){
+            window.alert("Date cannot be blank");
+        }
+
+
+
+
+    });
+
+    
+   
+
+
+
+
+
+
+
+
+
+});
