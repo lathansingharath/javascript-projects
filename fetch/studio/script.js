f57496cc2bcb09fde46ae9106ecf4c7f73a11a9a
Lathansingharath <singharath@hotmail.com>
2024-03-04 20:44:26 -0600

    done for now

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,43 @@
 //TODO: Add Your Code Below
+window.addEventListener("load",function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            console.log(json);
+            const astro = document.getElementById("bio");
+            let indexLength = json.length;
+            console.log(indexLength);
+            
+            for (let i = 0;i < indexLength;i++) {
+            astro.innerHTML += `
+                <div class="astronaut"> 
+                    <div class="bio"
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                         <ul>
+                          <li>Hours in space:${json[i].hoursInSpace}</li>
+                          <li>Active: ${json[i].active}</li>
+                          <li>Skills: ${json[i].skills}</li>
+                         </ul>
+                    </div>
+                    <img class="avatar" src=${json[i].picture}>
+                </div>
+               
+
+            `
+            }
+
+
+
+
+
+
+
+        })//end of response.json block
+        
+
+
+
+    })//end of fetch blcok
+
+
+
+})//end of the window.addEventListener block
